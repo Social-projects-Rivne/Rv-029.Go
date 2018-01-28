c888a01c13667978fd60f5bba469e6e472f192c4
zzell <sukhodolskyi.97@gmail.com>
2018-01-28 16:50:33 +0200

    add response handler

--- frontend/src/jsx/components/FormLogin.jsx
+++ frontend/src/jsx/components/FormLogin.jsx
@@ -7,6 +7,8 @@ import injectHash from '../decorators/hash.jsx';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import { SnackbarContent } from 'material-ui/Snackbar';
 import { withStyles } from 'material-ui/styles';
 
 class FormLogin extends Component {
@@ -17,7 +19,8 @@ class FormLogin extends Component {
       email: '',
       password: '',
       isValidEmail: true,
-      isValidPassword: true
+      isValidPassword: true,
+      errorMessage: null
     }
   }
 
@@ -27,7 +30,10 @@ class FormLogin extends Component {
     MD5hash: PropTypes.func.isRequired
   };
     
-  sendUserData = () => {
+  sendUserData = (e) => {
+    e.preventDefault();
+    
+    // TODO check validation onBlur
     if (!this.checkValidation()) return;
 
     const MD5 = this.props.MD5hash;
@@ -37,10 +43,21 @@ class FormLogin extends Component {
       password: MD5(this.state.password)
     })
     .then((res) => {
-      console.log(res)
+      console.log(res);
+      // TODO token
+      //localStorage.setItem('token', res.token);
     })
     .catch((err) => {
-      console.log(err)
+      switch (err.response.status) {
+      case 401:
+        this.setState({
+          errorMessage: err.response.data.Message
+        });
+        break;
+      default:
+        // TODO handle server errors
+        console.log(err);
+      }
     })
   };
   
@@ -51,8 +68,9 @@ class FormLogin extends Component {
     this.setState({
       isValidEmail: emailValidation,
       isValidPassword: passwordValidation
-    })
+    });
 
+    // false if at least one isn't correct
     return !!emailValidation && !!passwordValidation;
   };
 
@@ -69,31 +87,49 @@ class FormLogin extends Component {
   };
 
   render() {
+    const errorMessage = this.state.errorMessage;
+    
     return (
       <Paper
         className={this.props.classes.root}
         elevation={4}
         component='form'>
-
+        
+        <Typography
+          type='headline'
+          component='h3'>
+          Log In
+        </Typography>
+        
         <FormInput
           isValid={this.state.isValidEmail}
           type='text'
-          name='Email' 
+          name='Email'
           onUserInput={this.handleEmailInput} />
 
         <FormInput
           isValid={this.state.isValidPassword}
           type='password'
-          name='Password' 
+          name='Password'
           onUserInput={this.handlePasswordInput} />
 
         <Grid item xs={12}>
-          <Button raised color="primary"
-            onClick={this.sendUserData}>
-            Login
+          <Button
+            type='submit'
+            raised color='primary'
+            onClick={this.sendUserData}
+            className={this.props.classes.button}>
+            Submit
           </Button>
         </Grid>
-
+  
+        {/* TODO make banner more elegance */}
+        {errorMessage ? (
+          <Typography>
+            {errorMessage}
+          </Typography>
+        ):(null)}
+        
       </Paper>
     )
   }
@@ -101,11 +137,13 @@ class FormLogin extends Component {
 
 const styles = {
   root: {
-    padding: '3em 2em'
-  }
+    padding: '4em 3em'
+  },
+  button: {
+    marginTop: '1.5em',
+  },
 };
 
-// TODO looks like shit
 let FL = withStyles(styles)(FormLogin);
 let F = injectValidation(FL);
 export default injectHash(F);
