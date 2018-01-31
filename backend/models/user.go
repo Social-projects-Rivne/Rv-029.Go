301bd1b6730781a0f884d683fcfd8073b021ce35
Roman Hrab <lroman242@gmail.com>
2018-01-31 21:54:11 +0200

    send confirm link during registration

--- backend/models/user.go
+++ backend/models/user.go
@@ -24,6 +24,7 @@ type User struct {
 	Password  string
 	Salt      string
 	Role      string
+	Status    bool
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -31,8 +32,8 @@ type User struct {
 //Insert func inserts user object in database
 func (user *User) Insert() {
 
-	if err := db.Session.Query(`INSERT INTO users (id,email,first_name,last_name,password,salt,role,created_at,updated_at) VALUES (?,?,?,?,?,?,?,?,?);	`,
-		gocql.TimeUUID(), user.Email, user.FirstName, user.LastName, user.Password, user.Salt, user.Role, user.CreatedAt, user.UpdatedAt).Exec(); err != nil {
+	if err := db.Session.Query(`INSERT INTO users (id,email,first_name,last_name,password,salt,role,status,created_at,updated_at) VALUES (?,?,?,?,?,?,?,?,?);	`,
+		gocql.TimeUUID(), user.Email, user.FirstName, user.LastName, user.Password, user.Salt, user.Role, user.Status, user.CreatedAt, user.UpdatedAt).Exec(); err != nil {
 		fmt.Println(err)
 	}
 
@@ -40,21 +41,27 @@ func (user *User) Insert() {
 
 //FindByID func finds user from database
 func (user *User) FindByID(id string) error {
-	return db.Session.Query(`SELECT id, email, first_name, last_name, password, salt, role, created_at, updated_at FROM users WHERE id = ? LIMIT 1`,
-		id).Consistency(gocql.One).Scan(&user.UUID, &user.Email, &user.FirstName, &user.LastName, &user.Password, &user.Salt, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	return db.Session.Query(`SELECT id, email, first_name, last_name, password, salt, role, status, created_at, updated_at FROM users WHERE id = ? LIMIT 1`,
+		id).Consistency(gocql.One).Scan(&user.UUID, &user.Email, &user.FirstName, &user.LastName, &user.Password, &user.Salt, &user.Role, &user.Status, &user.CreatedAt, &user.UpdatedAt)
 }
 
 //FindByEmail func finds user from database
 func (user *User) FindByEmail(email string) {
 
-	if err := db.Session.Query(`SELECT id, email, first_name, last_name, password, salt, role, created_at, updated_at FROM users WHERE email = ? LIMIT 1 ALLOW FILTERING`,
-		email).Consistency(gocql.One).Scan(&user.UUID, &user.Email, &user.FirstName, &user.LastName, &user.Password, &user.Salt, &user.Role, &user.CreatedAt, &user.UpdatedAt); err != nil {
+	if err := db.Session.Query(`SELECT id, email, first_name, last_name, password, salt, role, status, created_at, updated_at FROM users WHERE email = ? LIMIT 1 ALLOW FILTERING`,
+		email).Consistency(gocql.One).Scan(&user.UUID, &user.Email, &user.FirstName, &user.LastName, &user.Password, &user.Salt, &user.Role, &user.Status, &user.CreatedAt, &user.UpdatedAt); err != nil {
 		fmt.Println(err)
 	}
 
 }
 
-//UpdateEmail func updates user's email in database(don't touch this shit)
+func (user *User) FindByToken (token string) error {
+	//TODO:
+
+	return nil
+}
+
+//UpdateEmail func updates user's email in database(FIXME: don't touch this shit)
 func (user *User) UpdateEmail() {
 
 	if err := db.Session.Query(`UPDATE example.users 
--- backend/router/router.go
+++ backend/router/router.go
@@ -29,10 +29,9 @@ func applyAuthRoutes(r *mux.Router)  {
 	r.HandleFunc("/register/", controllers.Register)
 	r.HandleFunc("/register", controllers.Register)
 
-	//r.HandleFunc("/logout", controllers.Logout)
+	r.HandleFunc("/confirm", controllers.ConfirmRegistration)
 	r.HandleFunc("/forget-password", controllers.ForgotPassword)
-	r.HandleFunc("/reset_password/{token}", controllers.ResetPassword)
-	//r.HandleFunc("/", controllers.ForgetPassword)
+	r.HandleFunc("/reset-password", controllers.ResetPassword)
 }
 
 func applyAuthorizedUserRoutes(r *mux.Router)  {
