19e740d72101a672d3a50c178fc4115eeb0b23cb
zzell <sukhodolskyi.97@gmail.com>
2018-02-22 10:51:33 +0200

    working on httptest

--- backend/controllers/board_test.go
+++ backend/controllers/board_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Social-projects-Rivne/Rv-029.Go/backend/mocks"
 	"net/http"
 	"net/http/httptest"
+	"github.com/gorilla/mux"
 )
 
 func TestDeleteBoard(t *testing.T) {
@@ -17,14 +18,16 @@ func TestDeleteBoard(t *testing.T) {
 	mockCRUD.EXPECT().Delete().Return(nil).Times(1)
 	testBoard.Delete()
 
-	req, err := http.NewRequest("DELETE", "/project/board/delete/{9325624a-0ba2-22e8-ba34-c06ebf83499a}", nil)
+	r := *mux.NewRouter()
+	res := httptest.NewRecorder()
+	req, err := http.NewRequest("DELETE", "/project/board/delete/9325624a-0ba2-22e8-ba34-c06ebf83499a/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	res := httptest.NewRecorder()
 	handler := http.HandlerFunc(DeleteBoard)
-	handler.ServeHTTP(res, req)
+	r.Handle("/project/board/delete/{board_id}/", handler).Methods("DELETE")
+	r.ServeHTTP(res, req)
 
 	if status := res.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
