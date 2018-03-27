1fa355bfd473c023904db9ad3dfd3401f91d005d
Roman Hrab <lroman242@gmail.com>
2018-03-27 14:09:13 +0300

    global socket connection with authorization

--- backend/scrum_poker/handler.go
+++ backend/scrum_poker/handler.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"reflect"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/Social-projects-Rivne/Rv-029.Go/backend/models"
 )
 
 var upgrader = websocket.Upgrader{
@@ -24,9 +26,60 @@ type SocketResponse struct {
 }
 
 var ActiveHubs = make(map[gocql.UUID]*Hub, 0)
+var ConnectedUsers = make(map[gocql.UUID]*Client, 0)
 
 func SocketHandler(w http.ResponseWriter, r *http.Request) {
 	conn, _ := upgrader.Upgrade(w, r, nil)
+	jwtToken := r.URL.Query().Get("token")
+
+	var keyFunc jwt.Keyfunc
+	keyFunc = func(token *jwt.Token) (interface{}, error) {
+		return "SomeSecretKey", nil
+	}
+
+	token, _ := jwt.Parse(jwtToken, keyFunc)
+	claims := token.Claims
+
+	if userID, ok := claims.(jwt.MapClaims)["UUID"]; ok {
+		userUUID, err := gocql.ParseUUID(userID.(string))
+		if err != nil {
+			conn.WriteJSON(SocketResponse{
+				Status: false,
+				Action: `CONNECTION`,
+				Message: `invalid token`,
+			})
+		}
+
+		user := models.User{
+			UUID: userUUID,
+		}
+		err = models.UserDB.FindByID(&user)
+		if err != nil {
+			conn.WriteJSON(SocketResponse{
+				Status: false,
+				Action: `CONNECTION`,
+				Message: `user not found`,
+			})
+		}
+
+		client := Client{}
+		client.conn = conn
+		client.send = make(chan []byte, 256)
+		client.user = &user
+		go client.WriteWorker()
+
+		ConnectedUsers[user.UUID] = &client
+
+		conn.WriteJSON(SocketResponse{
+			Status: true,
+			Action: `CONNECTION`,
+			Message: `you was successfully connected to the socket server`,
+		})
+
+		defer func() {
+			delete(ConnectedUsers, user.UUID)
+		}()
+	}
 
 	defer func() {
 		for _, hub := range ActiveHubs {
